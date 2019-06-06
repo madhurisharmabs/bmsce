c954eff336bb1ea5343f2f30f1cb513b35e6cfe0
madhurisharmabs <51438260+madhurisharmabs@users.noreply.github.com>
2019-06-06 15:55:16 +0530

    Update add of 2 num.c

--- add of 2 num.c
+++ add of 2 num.c
@@ -1,20 +1,25 @@
 #include<stdio.h>
+int getdata(int *a,int *b)
+{
+     printf("Enter the two numbers:");
+     scanf("%d%d",&a,&b);
+}
+int add(int a,int b)
+{
+	int c;
+	c=a+b;
+        return c;
+}
+int putdata(int c)
+{
+	printf("%d+%d=%d",c);
+}
+   
 int main()
 {
-  int a,b,sum;
-  int getdata(int a,int b)
-  {
-   printf("Enter the two numbers:");
-   scanf("%d%d",&a,&b);
-   }
-   int putdata(int sum)
-   {
-	printf("Sum of two num is %d",sum);
-   }
-
-   int add(int a,int b)
-   {
-	sum=a+b;
-   }
-   
+	int x,y,z;
+	getdata(x,y);
+        putdata(z);
+	z=add(x,y);
 }
+    
