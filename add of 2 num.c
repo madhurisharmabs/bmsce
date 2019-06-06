837bd34ac4ae1fecadd1b3e77421c57a1d87870e
madhurisharmabs <51438260+madhurisharmabs@users.noreply.github.com>
2019-06-06 14:21:49 +0530

    Create add of 2 num.c

---
+++ add of 2 num.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,sum;
+  int getdata()
+  {
+   printf("Enter the two numbers:");
+   scanf("%d%d",&a,&b);
+   }
+   int add()
+   {
+	sum=a+b;
+   }
+   int putdata()
+   {
+	printf("Sum of two num is",&sum);
+   }
+
+}
