94e937af4d468453c5830ea7a1f98ceaeb788edd
madhurisharmabs <51438260+madhurisharmabs@users.noreply.github.com>
2019-06-27 11:04:17 +0530

    Update add_2_frac_num.c

--- add_2_frac_num.c
+++ add_2_frac_num.c
@@ -5,14 +5,6 @@ struct Fraction
         int d;
 };
 int gcd(int a,int b)
-{#include<stdio.h>
-struct Fraction
-{
-        int n;
-        int d;
-};
-
-int gcd(int a,int b)
 {
         if(a>b)
           gcd(a-b,b);
@@ -20,7 +12,6 @@ int gcd(int a,int b)
           gcd(a,b-a);
         else
            return a;
-        //return 0;
 }
 void getdata(struct Fraction *f1,struct Fraction *f2)
 {
@@ -29,87 +20,36 @@ void getdata(struct Fraction *f1,struct Fraction *f2)
         printf("second fraction n2,d2:");
         scanf("%d%d",&f2->n,&f2->d);
 }
-int sum(struct Fraction f1,struct Fraction f2)
+void sum(struct Fraction f1,struct Fraction f2)
 {
         int a,b,lcm,x;
-        int temp1,temp2;
-        int res;
+        int temp1=0;
+        int temp2=0;
+        int ans=0;
         a=f1.d;
         b=f2.d;
         x=gcd(a,b);
         printf("The gcd of given num is %d\n",x);
         lcm=(f1.d*f2.d)/x;
         printf("The lcm of a num is %d\n",lcm);
-        temp1=((f1.n/f1.d)*lcm);
-        temp2=((f2.n/f2.d)*lcm);
-        res=temp1+temp2;
-        int ans=(res/lcm);
-
-        return ans;
-}
-void putdata(int ans)
-{
-        //int n1,n2;
-        printf("The sum of two fraction number is %d/%d",ans);
-
+        temp1=((f1.n*lcm)/f1.d);
+        temp2=((f2.n*lcm)/f2.d);
+        ans=(temp1+temp2);
+        printf("sum of 2 fraction  num is %d/%d",ans,lcm);
 }
 int main()
 {
         struct Fraction f1;
         struct Fraction f2;
-        int ans=0;
-        //int n1=0;
-        //int n2=0;
         getdata(&f1,&f2);
         sum(f1,f2);
-                                                                                            
-        if(a>b)
-            gcd(a-b,b);
-        else if(b>a)
-             gcd(a,b-a);
-        else
-             return a;
-}
-void getdata(struct Fraction *f1,struct Fraction *f2)
-{
-
-        printf("first fraction n1,d1:");
-        scanf("%d%d",&f1->n,&f1->d);
-        printf("second fraction n2,d2:");
-        scanf("%d%d",&f2->n,&f2->d);
-        //printf("The first and second fraction are %d/%d,%d/%d num :\n",f1->n,f1->d,f2->n,f2->d);
-
-}
-int sum(struct Fraction f1,struct Fraction f2)
-{
-        int a,b,lcm,x;
-        a=f1->d;
-        b=f2->d;
-        int ans=0;
-
-        x=gcd(a,b);
-        printf("The gcd of given num is %d\n",x);
-        lcm=(f1->d*f2->d)/x;
-        printf("The lcm of a num is %d\n",lcm);
-        ans=((f1->n*lcm)/f1->d)+((f2->n*lcm)/f2->d);
-
-        return ans;
-
-}
-
-void putdata(int ans)
-{
-        struct Fraction f1,f2;
-        printf("The sum of two fraction number %d/%d+%d/%d is %d/%d\n",f1.n,f1.d,f2.n,f2.d,ans);
-
-}
-int main()
-{
-        struct Fraction f1;
-        struct Fraction f2;
-        int ans=0;
-        getdata(&f1,&f2);
-        sum(f1,f2);
-        putdata(ans);
         return 0;
 }
+~
+~
+~
+~
+~
+~
+~
+~
