2270118f136364ebc76821228ff5d1643e3a7c55
madhurisharmabs <51438260+madhurisharmabs@users.noreply.github.com>
2019-06-27 20:10:31 +0530

    Update add_2_fraction_num.c

--- add_2_fraction_num.c
+++ add_2_fraction_num.c
@@ -3,44 +3,56 @@ struct Fraction
 {
         int n;
         int d;
-}
-void getdata()
-{
-        struct Fraction f1;
-        struct Fraction f2;
-        print("first fraction n1,d1",n/d);
-        scanf("%d %d",f1.n,f1.d);
-        printf("second fraction n2,d2",n/d);
-        scanf("%d %d",f2.n,f2.d);
+};
 
-}
-int gcd(int a, int b)
+int gcd(int a,int b)
 {
-    if (a == 0)
-        return b;
-    return gcd(b%a, a);
+        if(a>b)
+          gcd(a-b,b);
+        else if(b>a)
+          gcd(a,b-a);
+        else
+           return a;
 }
-
-void add()
+void getdata(struct Fraction *f1,struct Fraction *f2)
 {
-
+        printf("first fraction n1,d1:");
+        scanf("%d%d",&f1->n,&f1->d);
+        printf("second fraction n2,d2:");
+        scanf("%d%d",&f2->n,&f2->d);
 }
-void putdata(sum)
+int sum(struct Fraction f1,struct Fraction f2,int *ans,int *lcm)
 {
-        printf("The sum of two fraction number is %d",sum);
-
+        int a,b,x;
+        int temp1=0;
+        int temp2=0;
+        a=f1.d;
+        b=f2.d;
+        x=gcd(a,b);
+        printf("The gcd of given num is %d\n",x);
+        *lcm=(f1.d*f2.d)/x;
+        printf("The lcm of a num is %d\n",*lcm);
+        temp1=(((f1.n)* *lcm)/f1.d);
+        temp2=(((f2.n)* *lcm)/f2.d);
+        *ans=(temp1+temp2);
+//      printf("sum of 2 fraction  num is %d/%d",ans,lcm);
+        return *ans;
+}
+void putdata(int ans,struct Fraction f1,struct Fraction f2,int lcm)
+{
+        printf("The sum of two num is %d/%d+%d/%d=%d/%d",f1.n,f1.d,f2.n,f2.d,ans,lcm);
 }
 int main()
 {
-  struct f1,f2;
-  int x,y;
-  getdata();
-  gcd(x,y);
-  add();
-  putdata();
+        struct Fraction f1;
+        struct Fraction f2;
+        int ans=0;
+        int lcm=0;
+        getdata(&f1,&f2);
+        sum(f1,f2,&ans,&lcm);
+        putdata(ans,f1,f2,lcm);
+        return 0;
 }
 ~
 ~
-~
-~
-~
+"pro2.c" [unix] 55L, 
