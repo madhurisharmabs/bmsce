2335bc3c50baefe57e216f864caf7e2174b33b51
madhurisharmabs <51438260+madhurisharmabs@users.noreply.github.com>
2019-06-28 21:37:11 +0530

    Update add_2_frac_num.c

--- add_2_frac_num.c
+++ add_2_frac_num.c
@@ -4,10 +4,6 @@ struct Fraction
         int n;
         int d;
 };
-void getdata(struct Fraction *f1,struct Fraction *f2);
-int  gcd(int a,int b);
-int sum(struct Fraction,struct Fraction f2,int *ans,int *lcm);
-void putdata(int ans,struct Fraction f1,struct Fraction f2,int lcm);
 
 int gcd(int a,int b)
 {       if (b == 0)
@@ -22,9 +18,10 @@ void getdata(struct Fraction *f1,struct Fraction *f2)
         printf("second fraction n2,d2:");
         scanf("%d%d",&f2->n,&f2->d);
 }
-int sum(struct Fraction f1,struct Fraction f2,int *ans,int *lcm)
+int sum(struct Fraction f1,struct Fraction f2,struct  Fraction *res)
 {
         int a,b,x;
+        int *lcm=0;
         int temp1=0;
         int temp2=0;
         a=f1.d;
@@ -35,24 +32,27 @@ int sum(struct Fraction f1,struct Fraction f2,int *ans,int *lcm)
         printf("The lcm of a num is %d\n",*lcm);
         temp1=(((f1.n)* *lcm)/f1.d);
         temp2=(((f2.n)* *lcm)/f2.d);
-        *ans=(temp1+temp2);
-        return *ans;
+        res=(temp1+temp2);
+        return res;
 }
-void putdata(int ans,struct Fraction f1,struct Fraction f2,int lcm)
+void putdata(struct Fraction f1,struct Fraction f2,struct Fraction *res,int lcm)
 {
-        printf("The sum of two num is %d/%d+%d/%d=%d/%d",f1.n,f1.d,f2.n,f2.d,ans,lcm);
+        printf("The sum of two num is %d/%d+%d/%d=%d/%d",f1.n,f1.d,f2.n,f2.d,res,lcm);
 }
 int main()
 {
         struct Fraction f1;
         struct Fraction f2;
-        int ans=0;
+        struct Fraction res;
         int lcm=0;
         getdata(&f1,&f2);
-        sum(f1,f2,&ans,&lcm);
-        putdata(ans,f1,f2,lcm);
+        sum(f1,f2,&res);
+        putdata(f1,f2,&res,lcm);
         return 0;
 }
 ~
 ~
-"pro2.c" [unix] 55L, 1210C                                   
+~
+~
+~
+-- INSERT --                    
