b7cbff93f55b4a064280f20b9c9f83cf8747eb12
madhurisharmabs <51438260+madhurisharmabs@users.noreply.github.com>
2019-07-03 10:33:01 +0530

    Update add_2_fraction_num.c

--- add_2_fraction_num.c
+++ add_2_fraction_num.c
@@ -8,11 +8,18 @@ struct Fraction
 int gcd(int a,int b)
 {
         if(a>b)
-          gcd(a-b,b);
+        {
+         gcd(a-b,b);
+        }
         else if(b>a)
+        {
           gcd(a,b-a);
+        }
         else
-           return a;
+        {
+         return a;
+        }
+
 }
 void getdata(struct Fraction *f1,struct Fraction *f2)
 {
@@ -21,38 +28,31 @@ void getdata(struct Fraction *f1,struct Fraction *f2)
         printf("second fraction n2,d2:");
         scanf("%d%d",&f2->n,&f2->d);
 }
-int sum(struct Fraction f1,struct Fraction f2,int *ans,int *lcm)
+int sum(struct Fraction f1,struct Fraction f2,struct Fraction *res,int lcm)
 {
-        int a,b,x;
-        int temp1=0;
-        int temp2=0;
-        a=f1.d;
-        b=f2.d;
-        x=gcd(a,b);
-        printf("The gcd of given num is %d\n",x);
-        *lcm=(f1.d*f2.d)/x;
-        printf("The lcm of a num is %d\n",*lcm);
-        temp1=(((f1.n)* *lcm)/f1.d);
-        temp2=(((f2.n)* *lcm)/f2.d);
-        *ans=(temp1+temp2);
-//      printf("sum of 2 fraction  num is %d/%d",ans,lcm);
-        return *ans;
+        int a=f1.d;
+        int b=f2.d;
+        int gcdres=gcd(a,b);
+        printf("The gcd of a  given num is: %d\n",gcdres);
+        lcm=(f1.d*f2.d)/gcdres;
+        printf("The lcm of a given num is: %d\n",lcm);
+        res->n=((f1.n*lcm)/f1.d)+((f2.n*lcm)/f2.d);
+        res->d=(f1.d*f2.d)/gcdres;
+
+        return 0;
 }
-void putdata(int ans,struct Fraction f1,struct Fraction f2,int lcm)
+void putdata(struct Fraction f1,struct Fraction f2,struct Fraction *res)
 {
-        printf("The sum of two num is %d/%d+%d/%d=%d/%d",f1.n,f1.d,f2.n,f2.d,ans,lcm);
+        printf("The sum of two  fraction num is %d/%d+%d/%d=%d/%d",f1.n,f1.d,f2.n,f2.d,res->n,res->d);
 }
 int main()
 {
         struct Fraction f1;
         struct Fraction f2;
-        int ans=0;
+        struct Fraction res;
         int lcm=0;
         getdata(&f1,&f2);
-        sum(f1,f2,&ans,&lcm);
-        putdata(ans,f1,f2,lcm);
+        sum(f1,f2,&res,lcm);
+        putdata(f1,f2,&res);
         return 0;
-}
-~
-~
-"pro2.c" [unix] 55L, 
+-- INSERT --                                                   
