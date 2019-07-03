c9148ca7039617a4c90d9c68f0e9c1cedce4536c
madhurisharmabs <51438260+madhurisharmabs@users.noreply.github.com>
2019-07-03 09:34:15 +0530

    Update add_2_frac_num.c

--- add_2_frac_num.c
+++ add_2_frac_num.c
@@ -6,10 +6,16 @@ struct Fraction
 };
 
 int gcd(int a,int b)
-{       if (b == 0)
-           return a;
-        else
-           return gcd(b, a % b);
+{
+        if(a>b)
+        {
+         gcd(a-b,b);
+        }
+        else if(b>a)
+        {
+          gcd(a,b-a);
+        }
+        return a;
 }
 void getdata(struct Fraction *f1,struct Fraction *f2)
 {
@@ -18,26 +24,24 @@ void getdata(struct Fraction *f1,struct Fraction *f2)
         printf("second fraction n2,d2:");
         scanf("%d%d",&f2->n,&f2->d);
 }
-int sum(struct Fraction f1,struct Fraction f2,struct  Fraction *res)
+int sum(struct Fraction f1,struct Fraction f2,struct Fraction res)
 {
-        int a,b,x;
-        int *lcm=0;
-        int temp1=0;
-        int temp2=0;
+        int a,b;
+        int x;
         a=f1.d;
         b=f2.d;
         x=gcd(a,b);
-        printf("The gcd of given num is %d\n",x);
-        *lcm=(f1.d*f2.d)/x;
-        printf("The lcm of a num is %d\n",*lcm);
-        temp1=(((f1.n)* *lcm)/f1.d);
-        temp2=(((f2.n)* *lcm)/f2.d);
-        res=(temp1+temp2);
-        return res;
+        printf("The gcd of a  given num is: %d\n",x);
+        int lcm=0;
+        lcm=(f1.d * f2.d)/x;
+        printf("The lcm of a given num is: %d\n",lcm);
+        res.n=((f1.n * lcm)/f1.d)+((f2.n * lcm)/f2.d);
+        res.d=(f1.d * f2.d)/x;
+        return 0;
 }
-void putdata(struct Fraction f1,struct Fraction f2,struct Fraction *res,int lcm)
+void putdata(struct Fraction f1,struct Fraction f2,struct Fraction res,int lcm)
 {
-        printf("The sum of two num is %d/%d+%d/%d=%d/%d",f1.n,f1.d,f2.n,f2.d,res,lcm);
+        printf("The sum of two  fraction num is %d/%d+%d/%d=%d/%d",f1.n,f1.d,f2.n,f2.d,res,lcm);
 }
 int main()
 {
@@ -46,13 +50,9 @@ int main()
         struct Fraction res;
         int lcm=0;
         getdata(&f1,&f2);
-        sum(f1,f2,&res);
-        putdata(f1,f2,&res,lcm);
+        sum(f1,f2,res);
+        putdata(f1,f2,res,lcm);
         return 0;
 }
-~
-~
-~
-~
-~
--- INSERT --                    
+
+"pro2.c" [unix] 57L, 1021C                                                                     
